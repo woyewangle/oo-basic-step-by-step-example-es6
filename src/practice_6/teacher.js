9bbc5eb639bee13aedde53f6d02ea7c81c0fd96a
LEON YU (ITA-ISDC-ISD-OOCLL/ZHA) <YULE4@OOCL.COM>
2018-07-09 17:16:28 +0800

    teacher类继承person,抄写introduce

--- src/practice_6/teacher.js
+++ src/practice_6/teacher.js
@@ -1,1 +1,18 @@
 
+
+import Person from "../../src/practice_2/person.js";
+
+class Teacher extends Person{
+    constructor(name,age,klass){
+        super(name,age);
+        this.klass=klass;
+    }
+
+    introduce(){
+    if(this.klass!=null)
+       return super.introduce()+` I am a ${Teacher.name}. I teach Class ${this.klass}.`;
+    else
+       return super.introduce()+` I am a ${Teacher.name}. I teach No Class.`
+    }
+}
+module.exports = Teacher;
